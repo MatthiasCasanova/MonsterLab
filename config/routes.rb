b52779f6b9776b4b81e5f173cf181d7fb2bee720
MatthiasCasanova <matthiascasanova0311@gmail.com>
2023-03-02 16:09:52 +0100

    lancement monsterlab

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  resources :monster_lab_scaffolds
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
